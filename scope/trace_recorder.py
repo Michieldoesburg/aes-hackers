fe72861060abcbab03b4a0b32bcf5c40c64058dd
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2018-03-27 12:39:15 +0200

    minor changes.

--- scope/trace_recorder.py
+++ scope/trace_recorder.py
@@ -1,13 +1,14 @@
 import csv
 import os
 
-class trace_recorder(object):
 
+class trace_recorder(object):
     def __init__(self):
         super(trace_recorder, self).__init__()
         self.directory = "D:/Temp/traces"
         self.trace_count_filename = "trace_count.txt"
         self.trace_count = "1"
+        self.trace_count_file_path = self.directory + "/" + self.trace_count_filename
 
     def create_directory(self):
         if not os.path.exists(self.directory):
@@ -20,7 +21,7 @@ class trace_recorder(object):
             myfile.close()
 
         # Update the trace count number in the file.
-        with open(self.directory + "/" + self.trace_count_filename, 'w+') as file:
+        with open(self.trace_count_file_path, 'w+') as file:
             self.trace_count = str(int(self.trace_count) + 1)
             file.write(self.trace_count)
 
@@ -29,16 +30,16 @@ class trace_recorder(object):
         self.create_directory()
 
         # Check if the file that records the tracecount exists, if it doesn't then create it.
-        if not os.path.exists(self.directory + "/" + self.trace_count_filename):
-            file = open(self.directory + "/" + self.trace_count_filename, 'w')
+        if not os.path.exists(self.trace_count_file_path):
+            file = open(self.trace_count_file_path, 'w')
             file.write("1")
             file.close()
         else:
-            file = open(self.directory + "/" + self.trace_count_filename, 'r')
+            file = open(self.trace_count_file_path, 'r')
             self.trace_count = file.read()
             file.close()
 
 trace_recorder = trace_recorder()
 trace_recorder.setup()
 for x in range(0, 100):
-    trace_recorder.write_csv_file([1,2, 3, 4, 5])
+    trace_recorder.write_csv_file([1, 2, 3, 4, 5])
